384dcc5dca0882f2b421df6204ef2b41afba09d8
Marcos <marquinhos.dev@gmail.com>
2024-05-23 15:05:36 -0300

    ++ user session

---
+++ ClientLibrary/Services/Implementations/UserAccountService.cs
@@ -0,0 +1,29 @@
+﻿using BaseLibrary.DTOs;
+using BaseLibrary.Responses;
+using ClientLibrary.Services.Contracts;
+
+namespace ClientLibrary.Services.Implementations
+{
+    public class UserAccountService : IUserAccountService
+    {
+        public Task<GeneralResponse> CreateAsync(Register user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GeneralResponse> SignInAsync(Login user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponse> RefreshTokenAsync(RefreshToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<WeatherForecast[]> GetWeatherForecasts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
